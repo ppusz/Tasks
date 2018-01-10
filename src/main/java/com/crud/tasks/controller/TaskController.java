17f8d865139ade328787ec8d841094d1abbb540b
ppusz <pawel@pusz.name>
2018-01-10 12:33:19 +0100

    15.2 get task by id

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,8 +1,12 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,14 +16,25 @@ import java.util.List;
 @RequestMapping("/v1/task")
 public class TaskController {
 
+    @Autowired
+    DbService service;
+    @Autowired
+    TaskMapper taskMapper;
+
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask(String taskId) {
-        return new TaskDto((long)1, "test title", "test_content");
+    public TaskDto getTask(@RequestParam(value = "id", required = false) String taskId) {
+        try {
+            Long id = Long.parseLong(taskId);
+            return taskMapper.mapToTaskDto(service.getTask(id).get());
+        } catch (Exception e)
+        {
+            return null;
+        }
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
